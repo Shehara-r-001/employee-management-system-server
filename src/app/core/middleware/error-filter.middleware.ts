45cf0c30832e3f883041d729b68d115c892ccdc9
shehara_r <ranathisarashehara01@gmail.com>
2023-06-04 19:15:49 +0530

    update error filter

--- src/app/core/middleware/error-filter.middleware.ts
+++ src/app/core/middleware/error-filter.middleware.ts
@@ -2,7 +2,13 @@ import { NextFunction, Request, Response } from 'express'
 
 import { appResponse } from '../../shared/util/AppResponse'
 import { AppError } from '../../shared/util/AppError'
+import { CommonErrors } from '../../shared/enums/commonErrors.enum'
 
 export const errorFilter = (err: AppError | any, _req: Request, res: Response, _next: NextFunction) => {
-  appResponse.error(res, typeof err === typeof AppError ? err.httpCode : 500, err.name, err.message)
+  appResponse.error(
+    res,
+    typeof err === typeof AppError ? err.httpCode : 500,
+    typeof err === typeof AppError ? err.name : CommonErrors.INTERNAL_SERVER_ERROR,
+    err.message
+  )
 }
